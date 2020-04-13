babfb65aeff1551d804b4a7da93d96cb893598e2
lbw <xbl113lbw@163.com>
2020-04-13 17:12:26 +0800

    接口对接

--- src/views/home/home.vue
+++ src/views/home/home.vue
@@ -317,7 +317,7 @@
                         Toast("USDT余额不足");
                         return
                     }
-                    this.ajax.grt("v1/user/collide", {}).then(res => {
+                    this.ajax.get("v1/user/collide", {}).then(res => {
                         if (res.data.code === 200) {
                             Toast("成功");
                         }
